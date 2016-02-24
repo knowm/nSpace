352a750bfea2c7e0d32aaa4705875c3a3a43fa04
Jason Hoy <jasonmhoy@gmail.com>
2016-02-24 11:30:46 -0700

    Update #pragma for manifest for Windows platforms

--- c/app/nsh/main.cpp
+++ c/app/nsh/main.cpp
@@ -28,10 +28,11 @@ CCL_OBJLIST_END()
 
 // Prototypes
 
-// For 'modern'/XP look at feel
+// XP visual style
 #if defined(_WIN32) && !defined(_WIN64)
-//#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls'" \
-//					"version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
 #ifdef	_WIN32
