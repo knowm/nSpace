81cec12f93fbbfd0bba25915af60b69915fdd6e9
jasonmhoy <jasonmhoy@gmail.com>
2017-01-07 15:53:40 -0700

    Web link work.

--- resource/web/nspace_bind.js
+++ resource/web/nspace_bind.js
@@ -10,8 +10,8 @@
 // Create new instance of this object immediately
 new function ()
 	{
-	var	ws = null;										// WebSocket object
-	var	nElems = {};									// Bounded element states
+	var	ws			= null;								// WebSocket object
+	var	nElems	= {};									// Bounded element states
 
 	nSpace_bind =
 		{
@@ -30,6 +30,9 @@ new function ()
 			//		-	Initialize state of the binding object
 			//
 			////////////////////////////////////////////////////////////////////////
+
+			// Parser
+			nSpaceXML.init();
 
 			// Establish connection to webserver
 			ws					= new WebSocket("ws://localhost:8080/nspace");
@@ -57,7 +60,7 @@ new function ()
 			// Extract nSpace value from incoming XML
 			parser	= new DOMParser();
 			xmlDoc	= parser.parseFromString(event.data,"text/xml");
-			msg		= xmlToValue ( xmlDoc.documentElement );
+			msg		= nSpaceXML.load ( xmlDoc.documentElement );
 
 			// Debug
 			console.log(msg);
@@ -82,6 +85,18 @@ new function ()
 				if (loc == "element/enable/onfire/value")
 					elem.disabled = !msg["Value"];
 				}	// if
+
+			// Checkbox
+			else if (type == "checkbox")
+				{
+				// Enable
+				if (loc == "element/enable/onfire/value")
+					elem.disabled = !msg["Value"];
+
+				// Activate
+				else if (loc == "activate/onfire/value")
+					elem.checked = msg["Value"];
+				}	// else if
 
 			},
 
@@ -124,7 +139,7 @@ new function ()
 					nElems[path] = elems[i];
 
 					// Set-up events
-					elems[i].onclick = onClickElem;
+					elems[i].onclick = onClickn;
 
 					// Send listen request for path
 					listen(path);
@@ -170,202 +185,38 @@ new function ()
 
 		}	// listen
 
-	var xmlToValue = function(elem)
-		{
-		////////////////////////////////////////////////////////////////////////
-		//
-		//	PURPOSE
-		//		-	Convert XML document/node into a dictionary of values
-		//
-		//	PARAMETERS
-		//		-	elem is the XML element
-		//
-		// RETURN VALUE
-		//		Converted value
-		//
-		////////////////////////////////////////////////////////////////////////
-		var ret = null;
-
-		// Type
-		if (elem.nodeName == "Dictionary")
-			{
-			var key = null;
-			var val = null;
-
-			// Empty dictionary
-			var dct = {};
-
-			// Load key/value pairs into dictionary
-			for (var i = 0;i < elem.childNodes.length;i+=2)
-				if (	((key = xmlToValue(elem.childNodes[i+0])) != null) &&
-						((val = xmlToValue(elem.childNodes[i+1])) != null) )
-					dct[key] = val;
-
-			// Use as return value
-			ret = dct;
-			}	// if
-		else if (elem.nodeName == "List")
-			{
-			var val = null;
-
-			// Empty list
-			var lst = [];
-
-			// Load child values into list
-			for (var i = 0;i < elem.childNodes.length;++i)
-				if ((val = xmlToValue(elem.childNodes[i])) != null)
-					lst.push ( val );
-
-			// Use as return value
-			ret = lst;
-			}	// else if
-
-		// Single value
-		else if (elem.nodeName == "Value")
-			{
-			var val = null;
-
-			// Type specified ?
-			var type = (elem.attributes.length > 0) ?
-							elem.attributes["Type"].nodeValue : null;
-
-			// It is possible to receive an 'empty' value in which
-			// case there is not 'firstChild'.
-			if (	(elem.firstChild != null) &&
-					(val = elem.firstChild.nodeValue) != null)
-				{
-				// Default
-				if (type == null)
-					type = "string";
-				else 
-					type = type.toLowerCase();
-
-				// Convert type
-				if (type == "float" || type == "double")
-					val = parseFloat(val);
-				else if (type == "int" || type == "long")
-					val = parseInt(val);
-				else if (type == "boolean")
-					val = Boolean(val.toLowerCase() == "true");
-				}	// if
-
-			// Use as return value
-			ret = val;
-			}	// else if
-
-		return ret;
-		}	// xmlToValue
-
-	var valueToXml = function(value)
-		{
-		////////////////////////////////////////////////////////////////////////
-		//
-		//	PURPOSE
-		//		-	Convert value to XML.
-		//
-		//	PARAMETERS
-		//		-	value is the value to convert
-		//
-		// RETURN VALUE
-		//		XML string representing value
-		//
-		////////////////////////////////////////////////////////////////////////
-		var ret = null;
-		var dct = {};
-		var lst = [];
-
-		// Dictionary
-		if (value instanceof Object)
-			{
-			// Begin dictionary
-			ret = "<Dictionary>"
-
-			// Append values
-			for (var key in value)
-				{
-				// Key then value
-				ret += valueToXml(key);
-				ret += valueToXml(value[key]);
-				}	// for
-
-			// End dictionary
-			ret += "</Dictionary>";
-			}	// if
-
-		// List
-		else if (value instanceof Array)
-			{
-			console.log("Array");
-			}	// else if
-
-		// Value
-		else
-			{
-			// Begin value
-			ret = "<Value";
-
-			// String
-			console.log(typeof value);
-			if (typeof value == "string")
-				ret += ">"+value.toString();
-
-			// Number
-			else if (typeof value == "number")
-				{
-				// Integer
-				if (Number.isInteger(value))
-					{
-					ret += " Type=\"Integer\">";
-					ret += value.toString();
-					}	// else if
-				else
-					{
-					ret += " Type=\"Float\">";
-					ret += value.toString();
-					}	// else
-
-				}	// else if
-
-			// Boolean
-			else if (typeof value == "boolean")
-				{
-				ret += " Type=\"Boolean\">";
-				ret += value.toString();
-				}	// else if
-
-			// Default
-			else
-				ret += ">"+value.toString();
-
-			// End value
-			ret += "</Value>";
-			}	// else
-
-		return ret;
-		}	// valueToXml
-
 	//
 	// Events
 	//
 
-	// Buttons
-	var onClickElem = function(event)
+	var onClickn = function(event)
 		{
-		var dct  = {};
+		////////////////////////////////////////////////////////////////////////
+		//
+		//	PURPOSE
+		//		-	Called when an element is 'clicked'.
+		//
+		//	PARAMETERS
+		//		-	event contains the event information
+		//
+		////////////////////////////////////////////////////////////////////////
 		var type = event.srcElement.nodeName.toLowerCase(); 
+		var dct  = {};
+		var xml	= null;
 
 		// Button
+		alert ( type );
 		if (type == "button")
 			{
 			// Send activate value
 			dct["Verb"]			= "Store";
 			dct["Location"]	= event.srcElement.attributes["data-nabs"] + "Activate/OnFire/Value";
 			dct["Value"]		= 0;
-			var xml = valueToXml ( dct );
+			xml					= nSpaceXML.save(dct);
 			ws.send ( xml );
 			}	// if
 
-		}	// onClickElem
+		}	// onClickn
 
 	}	// function
 
